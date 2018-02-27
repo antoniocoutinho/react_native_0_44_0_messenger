e788975f604cfc88914927f74e8c172e26a05ec3
Antonio Rodrigues Coutinho <antoniorodrigues.coutinho@gmail.com>
2018-02-26 21:14:25 -0300

    feat: Add firebase module and configuration.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import Routes from './Routes';
 import reducers from './reducers';
+import firebase from 'firebase'
 
-export default props => (
-    <Provider store={createStore(reducers)}>
-        <Routes />
-    </Provider>
-);
+class App extends Component {
+    componentWillMount(){
+        let config = {
+            apiKey: "AIzaSyD3J0p9je0XXLhjQ-JuhkGzGrLT7XnfB1s",
+            authDomain: "react-native-messenger-81faa.firebaseapp.com",
+            databaseURL: "https://react-native-messenger-81faa.firebaseio.com",
+            projectId: "react-native-messenger-81faa",
+            storageBucket: "react-native-messenger-81faa.appspot.com",
+            messagingSenderId: "725048052656"
+          };
+          firebase.initializeApp(config);
+    }
+
+    render () {
+        return (
+
+            <Provider store={createStore(reducers)}>
+                <Routes />
+            </Provider>
+
+        ) 
+    }
+}
+;
+export default App;
